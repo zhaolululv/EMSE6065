c25a19eb018ad3f7ee9451dec43fb1424bd07219
Zhao <lujin@gwu.edu>
2021-10-13 16:35:33 -0400

    Adding image to survey table

--- 6-conjoint-questions/make_choice_questions.R
+++ 6-conjoint-questions/make_choice_questions.R
@@ -3,7 +3,7 @@
 # Install packages
 # install.packages("fastDummies")
 # install.packages("remotes")
-# remotes::install_github("jhelvy/conjointTools")
+#remotes::install_github("jhelvy/conjointTools")
 
 # Load libraries
 library(tidyverse)
@@ -16,8 +16,8 @@ levels <- list(
   price       = seq(5000,50000,1000),   # Price ($1,000)
   #fuelEconomy = c(20, 25, 30),   # Fuel economy (mpg)
   modelyear   = c(1, 2, 3,4,5),      # 0-60 mph acceleration time (s)
-  powertrain  = c("Gasoline", "Electric"), 
-  brand       = c("Honda","Tesla","Mercedes"),
+  powertrain  = c("Gas", "Electric"), 
+  brand       = c("Nissan","Tesla","Mercedes"),
   Warranty    = c("Yes","No"),
   range       = c(100, 150, 200, 250), # EV driving range
   Mileage     = seq(5000,50000,1000),
@@ -35,13 +35,13 @@ head(doe) # preview
 
 
 # Make a basic survey
-survey <- makeSurvey(
-    doe       = doe,  # Design of experiment
-    nResp     = 1000, # Total number of respondents (upper bound)
-    nAltsPerQ = 2,    # Number of alternatives per question
-    nQPerResp = 4     # Number of questions per respondent
-)
-head(survey) # preview
+# survey <- makeSurvey(
+#     doe       = doe,  # Design of experiment
+#     nResp     = 1000, # Total number of respondents (upper bound)
+#     nAltsPerQ = 2,    # Number of alternatives per question
+#     nQPerResp = 4     # Number of questions per respondent
+# )
+# head(survey) # preview
 
 # Make a labeld survey with "powertrain" as the label
 survey_labeled <- makeSurvey(
@@ -55,18 +55,27 @@ head(survey_labeled) # preview
 
 survey_labeled <- dummy_cols(survey_labeled,"powertrain")
 survey_labeled <- survey_labeled %>%
-  mutate(ChargingStation=ifelse(powertrain_Electric==1,ChargingStation,NA))
+  mutate(ChargingStation=ifelse(powertrain_Electric==1,ChargingStation,NA),
+         image = case_when(
+             (powertrain=="Electric"&brand=="Nissan") ~ "Nissan_Leaf.jpg",
+             powertrain=="Gas"&brand=="Nissan" ~ "Nissan_Gas.jpg",
+             powertrain=="Electric"&brand=="Mercedes" ~ "MB_EQ.jpg",
+             powertrain=="Gas"&brand=="Mercedes" ~ "MB_C300.jpg",
+             brand=="Tesla" ~ "Tesla_Model_3.jpg"
+             
+         ))
 head(survey_labeled)
 
 # Make a survey with outside good
-survey_og <- makeSurvey(
-    doe       = doe,
-    nResp     = 1000,
-    nAltsPerQ = 3,
-    nQPerResp = 8,
-    outsideGood = TRUE
-)
-head(survey_og) # preview
+# survey_og <- makeSurvey(
+#     doe       = doe,
+#     nResp     = 1000,
+#     nAltsPerQ = 3,
+#     nQPerResp = 8,
+#     outsideGood = TRUE
+# )
+# head(survey_og) # preview
 
 # Save design
 write_csv(survey, here('choice_questions.csv'))
+
