005e0639eb5efc9e5a478c8f44cc74fd15be5165
zhaolululv <lujin@gwu.edu>
2021-10-27 19:13:24 -0400

    data correction

--- make_choice_questions.R
+++ make_choice_questions.R
@@ -15,14 +15,13 @@ library(here)
 # Define the attributes and levels
 levels <- list(
   price       = seq(5000,50000,1000),   # Price ($1,000)
-  #fuelEconomy = c(20, 25, 30),   # Fuel economy (mpg)
-  modelyear   = c(1, 2, 3,4,5),      # 0-60 mph acceleration time (s)
+  fuelEconomy = seq(15,35,5),   # Fuel economy (mpg)
+  modelyear   = seq(2015,2020,1),
   powertrain  = c("Gas", "Electric"), 
-  brand       = c("Nissan","Tesla","Mercedes"),
   Warranty    = c("Yes","No"),
   range       = c(100, 150, 200, 250), # EV driving range
   Mileage     = seq(5000,50000,1000),
-  ChargingStation = c("free","discounted","paid")
+  ChargingStation = c("Free","Discounted","No Promotion")
   )
 
 # Make a full-factorial design of experiment
@@ -48,29 +47,18 @@ head(doe) # preview
 survey_labeled <- makeSurvey(
     doe       = doe,
     nResp     = 10000,
-    nAltsPerQ = 2,
+    nAltsPerQ = 3,
     nQPerResp = 4,
-    group     = "powertrain"
+    #group     = "powertrain"
 )
 head(survey_labeled) # preview
 
 survey_labeled <- dummy_cols(survey_labeled,"powertrain")
-wrong_doe <- survey_labeled %>% 
-    filter(brand=="Tesla"&powertrain=="Gas") %>% 
-    select(respID)
-wrong_respID <- unique(wrong_doe$respID )
 
 survey_labeled <- survey_labeled %>%
   mutate(ChargingStation=ifelse(powertrain_Electric==1,ChargingStation,0),
-         image = case_when(
-             (powertrain=="Electric"&brand=="Nissan") ~ "Nissan_Leaf.jpg",
-             powertrain=="Gas"&brand=="Nissan" ~ "Nissan_Gas.jpg",
-             powertrain=="Electric"&brand=="Mercedes" ~ "MB_EQ.jpg",
-             powertrain=="Gas"&brand=="Mercedes" ~ "MB_C300.jpg",
-             brand=="Tesla" ~ "Tesla_Model_3.jpg"
-             
-         )) %>% 
-    filter(respID %notin% wrong_respID)
+         range=ifelse(powertrain_Electric==1,range,0),
+         fuelEconomy=ifelse(powertrain_Electric==0,fuelEconomy,0))
 
 
 head(survey_labeled)
